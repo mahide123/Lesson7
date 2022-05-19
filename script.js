c12255d772c1db966ebcead00ad2f1c9ad4fc408
Mahide Rubio <mahide123@gmail.com>
2022-05-19 08:15:14 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+let toggle = false;
+
 const remove = (elem) => {
     elem.style.transform = 'translateX(-9999px)';
+    elem.style.transition = 'all 3s';
+}
+
+const add = (elem) => {
+    elem.style.transform = 'translateX(0)';
+    elem.style.transition = 'all 500ms';
 }
 
 
 btn.onclick = function() {
-    remove(text);
-    alert(`Now You Don't`);
+    if (toggle === false) {
+        remove(text);
+        alert(`Now you don't.`);
+        toggle = true;
+    } else {
+        add(text);
+        alert(`I am back.`);
+        toggle = false;
+    }
 }
